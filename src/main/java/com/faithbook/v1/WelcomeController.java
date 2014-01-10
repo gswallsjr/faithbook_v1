5c07fce32ea8c234880f7ba099e3877b01ac5a73
gswallsjr <g.wallsjr@gmail.com>
2014-01-10 00:12:12 -0500

    Updating Welcome Controller

--- src/main/java/com/faithbook/v1/WelcomeController.java
+++ src/main/java/com/faithbook/v1/WelcomeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class WelcomeController {
 	
-	@RequestMapping(value="/welcome", method = RequestMethod.GET)
+	@RequestMapping(value="/", method = RequestMethod.GET)
 	public String serveWelcome(ModelMap map, Principal principal){
 		
 		String name = "";
