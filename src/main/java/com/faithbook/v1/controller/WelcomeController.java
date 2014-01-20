bed4406beab0a6267a56a3f7fc7af17a1aa23be5
gswallsjr <g.wallsjr@gmail.com>
2014-01-20 17:02:49 -0500

    Adding Context Mapping

--- src/main/java/com/faithbook/v1/controller/WelcomeController.java
+++ src/main/java/com/faithbook/v1/controller/WelcomeController.java
@@ -10,6 +10,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class WelcomeController {
 	
+	@RequestMapping(value="/", method = RequestMethod.GET)
+	public String serveContext(ModelMap map, Principal principal){
+		
+		String name = "";
+		
+		if (null != principal)
+			name = principal.getName();
+		else
+			name = "Anonymous";
+		
+		
+		map.addAttribute("message", "Welcome to the Future of Meetings");
+		map.addAttribute("username", name);
+		
+		return "/public/welcome";	
+	}
+	
 	@RequestMapping(value="/public/", method = RequestMethod.GET)
 	public String serveWelcome(ModelMap map, Principal principal){
 		
